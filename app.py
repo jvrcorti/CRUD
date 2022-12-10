fcb70f2a494090cccc1b10a7c064fdb8e81d348c
Javier <jvrcorti@gmail.com>
2022-12-09 23:49:24 -0300

    Sincronizar base de datos etapa uno

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
 import os
 
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
@@ -8,33 +8,31 @@ app = Flask(__name__)
 
 # Database connection
 mysql = MySQL()
-app.config['MYSQL_DATABASE_HOTS']='localhost'
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']=''
-app.config['MYSQL_DATABASE_BD']='sistemas'
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='sistemas'
 #app.config['MYSQL_DATABASE_']
-mysql.init_app(app)
+#mysql.init_app(app)
+mysql = MySQL(app)
 
 # Path the application 
 @app.route('/')
 def index():
     return render_template('empleados/index.html')
 
-@app.route('/add_contact', methods=['POST'])
+@app.route('/add_contact', methods=['POST', 'GET'])
 def add_contact():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
         foto = request.form['foto']
-        sql = "INSERT INTO `sistemas`.`empleados` (`id`, `Nombre`, `Email`, `Foto`) VALUES (%s, %s, %s, %s)', (NULL, name, email, foto)"
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        conn.commit()
-        print(name)
-        print(email)
-        print(foto)
-        return 'recivided'
+        cursor = mysql.connection.cursor()
+        cursor.execute(' INSERT INTO empleados VALUES(NULL, %s, %s, %s)',(name, email, foto))
+        mysql.connection.commit()
+        cursor.close()
+        print(name, email, foto)
+        return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
